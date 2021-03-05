ad8fc33a982400e7baf316e49bbb7a386f522c53
Sukhdeep kaur <sukhdeep_2929@yahoo.com>
2021-03-04 16:45:46 -0800

    Add skeleton for functions

---
+++ R/countpoints.R
@@ -0,0 +1,19 @@
+#' Tally Available Points in Lab
+#'
+#'
+#' @param margins A scalar logical which indicates whether to add a row for the
+#'   total number of points. Defaults to TRUE.
+#' @inheritParams parse_lab
+#'
+#'
+#' @return A dataframe indicating total optional and required number of points.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' count_points()
+#' }
+count_points <- function(notebook, margins = TRUE) {
+
+}
+
---
+++ R/labchecks.R
@@ -0,0 +1,58 @@
+
+#' Check whether the user has included the github repo link in his/her repository
+#'
+#' @inheritParams parse_lab
+#' @return A logical which indicates whether the repo link exists or not
+#'
+#'
+#' @examples
+#' \dontrun{
+#' check_repo_link()
+#' }
+check_repo_link <- function(notebook) {
+
+}
+
+#' Check whether the user has pushed the latest version in his/her repository
+#'
+#' @inheritParams parse_lab
+#' @return A logical which indicates whether the last pushed version is latest or not
+#'
+#'
+#' @examples
+#' \dontrun{
+#' check_lat_version()
+#' }
+check_lat_version <- function(notebook) {
+
+}
+
+#' Check whether the user has at least three commits
+#' @inheritParams parse_lab
+#' @return A logical which indicates whether the repo has 3 commits or not
+#'
+#'
+#' @examples
+#' \dontrun{
+#' check_commits()
+#' }
+check_commits <- function(notebook) {
+
+}
+
+#' Performs Mechanics Checks on a MDS Lab This function check that you have a
+#' Github repo link, that you have pushed your latest commit, and that you have
+#' at least three commit messages authored by you in your history.
+#'
+#' @inheritParams parse_lab
+#' @return The function prints the results of the mechanics checks to screen.
+#'  Silently returns TRUE if all the checks are passed.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' check_mechanics()
+#' }
+check_mechanics <- function(notebook) {
+
+}
