ac785082924f87aa3924d588f1023c3317bb854f
Sukhdeep Kaur <70689363+sukh2929@users.noreply.github.com>
2021-03-04 17:16:04 -0800

    Add a skeleton to the package functions (#9)
    * Remove extra quotation mark
    
    * Add skeleton for functions
    
    * Make the docstring more descriptive
    
    Co-authored-by: Kamal Moravej <70611780+kmoravej@users.noreply.github.com>
    
    Co-authored-by: Kamal Moravej <70611780+kmoravej@users.noreply.github.com>

---
+++ R/parse.R
@@ -0,0 +1,15 @@
+#' Parse MDS lab files to return the markdown content
+#'
+#' @param notebook A character indicating the path or list of paths to MDS lab
+#'   files (either.ipynb or .Rmd). If left blank, the function will recursively
+#'   search for all labs in the working directory based on the file extension.
+#'
+#' @return A character vector where each element is the content of one markdown cell.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' parse_lab()
+#' }
+parse_lab <- function(notebook = NULL) {
+}
