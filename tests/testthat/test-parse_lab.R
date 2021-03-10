fa0d08f74e5718743809c04da3d970fb0ee0d479
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-03-09 21:49:55 -0800

    Add lab dummy files for testing

---
+++ tests/testthat/test-parse_lab.R
@@ -0,0 +1,11 @@
+test_that("Dummy test files must be readable", {
+  # Set paths to dummy lab files
+  py_file <- system.file("extdata", "dummylab.ipynb", package = "labzenr")
+  r_file  <- system.file("extdata", "dummylab.Rmd", package = "labzenr")
+
+  # check they are accessible
+  expect(fs::file_exists(py_file),
+         failure_message = "dummylab.ipynb not found")
+  expect(fs::file_exists(py_file),
+         failure_message = "dummylab.Rmd not found")
+})
